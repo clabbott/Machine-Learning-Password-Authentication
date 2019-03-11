b3ae98adb606ce39039cbbe2672107e4378636d9
mikeoa13 <47721440+mikeoa13@users.noreply.github.com>
2019-03-11 19:40:30 -0400

    Add files via upload

---
+++ GitHub/Biometrics.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Biometrics {
+	ArrayList<Integer> keyPresses;
+	ArrayList<String> keyPressesStrings;
+	ArrayList<Long> timePressedList;
+	ArrayList<Long> timeBetweenPressesList;
+
+	public Biometrics() {} 
+
+	public Biometrics(ArrayList<Integer> keyPresses, ArrayList<String> keyPressesStrings, ArrayList<Long> timePressedList, ArrayList<Long> timeBetweenPressesList) {
+		this.keyPresses = keyPresses;
+		this.keyPressesStrings = keyPressesStrings;
+		this.timePressedList = timePressedList;
+		this.timeBetweenPressesList = timeBetweenPressesList;
+	}
+
+	public String toString() {
+		String press1 = new String(keyPressesStrings.get(0) + ": " + timePressedList.get(0));
+		String between1 = new String("Time between " + keyPressesStrings.get(0) + " and " + keyPressesStrings.get(1) + ": " + timeBetweenPressesList.get(0));
+		String press2 = new String(keyPressesStrings.get(1) + ": " + timePressedList.get(1));
+		String between2 = new String("Time between " + keyPressesStrings.get(1) + " and " + keyPressesStrings.get(2) + ": " + timeBetweenPressesList.get(1));
+		String press3 = new String(keyPressesStrings.get(2) + ": " + timePressedList.get(2));
+		String between3 = new String("Time between " + keyPressesStrings.get(2) + " and " + keyPressesStrings.get(3) + ": " + timeBetweenPressesList.get(2));
+		String press4 = new String(keyPressesStrings.get(3) + ": " + timePressedList.get(3));
+		String between4 = new String("Time between " + keyPressesStrings.get(3) + " and " + keyPressesStrings.get(4) + ": " + timeBetweenPressesList.get(3));
+		String press5 = new String(keyPressesStrings.get(4) + ": " + timePressedList.get(4));
+		String result = "\n" +  press1 + "\n" + between1 + "\n" + press2 + "\n" + between2 + "\n" + press3 + "\n" + between3 + "\n" + press4 + "\n" + between4 + "\n" + press5;
+		return result;
+	}
+}
--- GitHub/UserInput.java
+++ GitHub/UserInput.java
@@ -1,26 +1,208 @@
-package org.deeplearning4j.examples.feedforward.classification.detectgender.GitHub;
-
-
+import java.awt.FlowLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class UserInput implements KeyListener {
-//    private long timePressed;
-//    private long timeReleased;
-//    private ArrayList<Long> timePressedLeft;
-//    private ArrayList<Long> timePressedRight;
-//    private ArrayList<Long> timePressedUp;
-//    private ArrayList<Long> timePressedDown;
-//    private long startTime;
-//    private long endTime;
+import javax.swing.JTextField;
 
-    public void keyPressed(KeyEvent e){
+import java.util.ArrayList;
+
+public class UserInput extends JFrame implements KeyListener {
+
+	private static final long serialVersionUID = 1L;
+	
+	JLabel label;
+    JTextField txtOne;
+	JTextField txtPassword;
+	
+	ArrayList<Integer> keyPresses;
+	ArrayList<String> keyPressesStrings;
+	long timePressed;
+	long timeReleased;
+
+	ArrayList<Long> totalTimePressedList;
+	ArrayList<Long> timePressedList;
+	ArrayList<Long> timeBetweenPressesList;
+
+	Biometrics biometrics;
+
+    public UserInput(String s) {
+        super(s);
+        JPanel p = new JPanel();
+        label = new JLabel("Type a 5-character password and press enter when you're done: ");
+        p.add(label);
+        add(p);
+        addKeyListener(this);
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        
+        keyPresses = new ArrayList<Integer>();
+		keyPressesStrings = new ArrayList<String>();
+		totalTimePressedList = new ArrayList<Long>();
+		timePressedList = new ArrayList<Long>();
+		timeBetweenPressesList = new ArrayList<Long>();
+        
+        setVisible(true);
+    }
+    
+    public Biometrics end() {
+    	timeBetweenPressesList = getTimeBetweenPresses(timePressedList);
+    	biometrics = new Biometrics(keyPresses, keyPressesStrings, totalTimePressedList, timeBetweenPressesList);
+    	System.out.print(biometrics.toString());
+    	return biometrics;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    	if (e.getKeyCode() == KeyEvent.VK_A) {
+            System.out.println("A key TYPED");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Right key typed");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Left key typed");
+        }
 
     }
-    public void keyReleased(KeyEvent e){
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+    	int key = e.getKeyCode();
+		keyPresses.add(key);
+		timePressed = System.nanoTime();
+		timePressedList.add(timePressed);
     }
-    public void keyTyped(KeyEvent e){
 
+    @Override
+    public void keyReleased(KeyEvent e) {
+    	timeReleased = System.nanoTime();
+		totalTimePressedList.add(timeReleased - timePressed);
+		
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+            System.out.print("a");
+            keyPressesStrings.add("a");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_B) {
+            System.out.print("b");
+            keyPressesStrings.add("b");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_C) {
+            System.out.print("c");
+            keyPressesStrings.add("c");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_D) {
+            System.out.print("d");
+            keyPressesStrings.add("d");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_E) {
+            System.out.print("e");
+            keyPressesStrings.add("e");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_F) {
+            System.out.print("f");
+            keyPressesStrings.add("f");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_G) {
+            System.out.print("g");
+            keyPressesStrings.add("g");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_H) {
+            System.out.print("h");
+            keyPressesStrings.add("h");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_I) {
+            System.out.print("i");
+            keyPressesStrings.add("i");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_J) {
+            System.out.print("j");
+            keyPressesStrings.add("j");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_K) {
+            System.out.print("k");
+            keyPressesStrings.add("k");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_L) {
+            System.out.print("l");
+            keyPressesStrings.add("l");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_M) {
+            System.out.print("m");
+            keyPressesStrings.add("m");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_N) {
+            System.out.print("n");
+            keyPressesStrings.add("n");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_O) {
+            System.out.print("o");
+            keyPressesStrings.add("o");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_P) {
+            System.out.print("p");
+            keyPressesStrings.add("p");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_Q) {
+            System.out.print("q");
+            keyPressesStrings.add("q");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_R) {
+            System.out.print("r");
+            keyPressesStrings.add("r");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_S) {
+            System.out.print("s");
+            keyPressesStrings.add("s");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_T) {
+            System.out.print("t");
+            keyPressesStrings.add("t");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_U) {
+            System.out.print("u");
+            keyPressesStrings.add("u");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_V) {
+            System.out.print("v");
+            keyPressesStrings.add("v");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_W) {
+            System.out.print("w");
+            keyPressesStrings.add("w");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_X) {
+            System.out.print("x");
+            keyPressesStrings.add("x");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_Y) {
+            System.out.print("y");
+            keyPressesStrings.add("y");
+        }
+    	if (e.getKeyCode() == KeyEvent.VK_Z) {
+            System.out.print("z");
+            keyPressesStrings.add("z");
+    	}
+    	if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+    		end();
+    	}
+    }
+
+    public ArrayList<Long> getTimeBetweenPresses(ArrayList<Long> timePressedList) {
+		for (int i = 1; i < timePressedList.size(); i++) {
+			timeBetweenPressesList.add(timePressedList.get(i) - timePressedList.get(i - 1));
+		}
+		return timeBetweenPressesList;
+	}
+    
+    public static void main(String[] args) {
+        new UserInput("Key Listener Tester");
     }
 }
