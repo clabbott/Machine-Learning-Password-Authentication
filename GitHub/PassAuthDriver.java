d2cf7176f183157b2d3e6c5d028b79f4c80f748b
Clark Abbott <clark.a.abbott@gmail.com>
2019-03-13 00:29:11 -0400

    Demo v.0

--- GitHub/PassAuthDriver.java
+++ GitHub/PassAuthDriver.java
@@ -1,38 +1,90 @@
 package org.deeplearning4j.examples.feedforward.classification.detectgender.GitHub;
 
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PassAuthDriver  {
 
     public static void main(String[] args){
-        //Get user-entered username
+        //To do:
+        // find a way to get data from userinput2 into clarkData.csv
+        // relay that csv to the NN
+        // delete userinput, rename userinput2
+        // make it easy to export and redo on pi for mike
+        // put code on github
 
-        //Compare username to existing usernames, determines user
-        //May need to create user class to store user data or just pass username as a string
+        UserInput2 test = new UserInput2("Key Listener Tester");
 
-        //Get user-entered password while taking time key were pressed down
-        //Store time press of values in an array/ArrayList
-        //Will always only be 5 key presses
-        //Somehow convert this to a value understandable by NN
+        try{
+            Thread.sleep(10000);
+        }catch(Exception e){System.out.println("error");}
 
-        //Compares values of user-entered password to password
+        Biometrics data = test.end();
 
-        //Calls passAuthNN with parameters String username, (Arrsy) Passvalues
+        if(!data.keyPressesStrings.toString().equals("[c, l, a, r, k]")){
+            System.out.println("");
+            System.out.println("Incorrect password.");
+            return;
+        }else {
+            System.out.println("");
+            System.out.println("Correct password! Time for step 2.");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println("error");
+            }
+        }
 
-        //Fix this. Add second parameter with array of 9 string values
-        UserInput.writeDataLineByLine("C:\\Users\\Clark Abbott\\dl4j-examples\\dl4j-examples\\src\\main\\resources" +
-            "\\classification\\clarkData.csv");
+        ArrayList<Long> placeholder = new ArrayList<>();
+        placeholder.add((long) 0);
 
-        //Assign the user above a label according to their username.
+        for (Long elem:data.timePressedList){
+            placeholder.add(elem);
+        }
+        for (Long elem:data.timeBetweenPressesList.subList(0,5)){
+            placeholder.add(elem);
+        }
+        System.out.println(placeholder);
 
+        String[] arr = new String[placeholder.size()];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = placeholder.get(i).toString();
+            System.out.println(placeholder.get(i));
+        }
+
+        File file = new File("C:\\Users\\Clark Abbott\\dl4j-examples\\dl4j-examples\\src\\main\\resources" +
+            "\\classification\\passData.csv");
+        try {
+            // create FileWriter object with file as parameter
+            FileWriter outputfile = new FileWriter(file);
+
+            // create CSVWriter object filewriter object as parameter
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            // add data to csv
+            //Make this take in data from the passed parameter insted of just using this
+            writer.writeNext(arr);
+
+            // closing writer connection
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("You done fucked up now");
+        }
 
         try{
             System.out.println("Running Password Authentication now.");
 
-            //Pass expected label along too
             boolean val = PasswordAuthentication.NeuralNetwork();
             System.out.println("Neural Network returned " + val);
         }catch (Exception e){
             System.out.println("Unexpected Error:" +e);
         }
-
     }
 }
